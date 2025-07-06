443aaba367bac7b9a5b76418108722df694bc436
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-07-06 16:03:25 +0530

    Implemnted and saw what is layout file what it does and designed a simple dashboard, aside and navbar using layout

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,10 +1,26 @@
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div>
-      <nav>
-        <h1>NAVBAR</h1>
+    <div className="h-screen flex flex-col">
+      <nav className="bg-black flex items-center justify-between text-white font-semibold py-4 px-5 ">
+        <h1 className="text-xl ">LOGO</h1>
+        <ul className="flex items-center gap-8">
+          <li>Home</li>
+          <li>About us</li>
+          <li>Contact us</li>
+          <li>Login</li>
+        </ul>
       </nav>
-      <div>{children}</div>
+      <div className="flex flex-grow">
+        <aside className=" border-r border-r-gray-400 w-[15vw]">
+          <ul className="flex flex-col gap-6 text-md font-semibold text-gray-600 px-10 pt-8 w-full">
+            <li>Dashboard</li>
+            <li>Credits</li>
+            <li>Payments</li>
+            <li>Profile</li>
+          </ul>
+        </aside>
+        <div className="p-4">{children}</div>
+      </div>
     </div>
   );
 };
