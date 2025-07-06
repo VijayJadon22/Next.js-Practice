5a65c813a1e2a5bb9dd729c9d3a2e3582a1b4e9f
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-07-06 17:58:16 +0530

    Initial commit

--- app/dashboard/profile/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const ProfilePage = () => {
-  return <div className="text-2xl font-bold">This is Profile Page</div>;
-};
-
-export default ProfilePage;
--- app/dashboard/user/page.tsx
+++
@@ -1,9 +0,0 @@
-const waitFor = (sec: number) =>
-  new Promise((res, rej) => setTimeout(res, sec * 1000));
-const UserPage = async () => {
-  await waitFor(3);
-  //   console.log(message);
-  return <h1 className="text-2xl font-bold">This is UserPage</h1>;
-};
-
-export default UserPage;
