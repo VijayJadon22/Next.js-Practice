c011af63a3f9e351f260e6ccf974667c47dc0275
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-07-06 17:37:24 +0530

    Used Link that nest/link provides us for navigation

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-screen flex flex-col">
@@ -13,13 +15,21 @@ const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
       <div className="flex flex-grow">
         <aside className=" border-r border-r-gray-400 w-[15vw]">
           <ul className="flex flex-col gap-6 text-md font-semibold text-gray-600 px-10 pt-8 w-full">
-            <li>Dashboard</li>
-            <li>Credits</li>
-            <li>Payments</li>
-            <li>Profile</li>
+            <li>
+              <Link href={"/dashboard"}>Dashboard</Link>
+            </li>
+            <li>
+              <Link href={"/dashboard/todo"}>Todos</Link>
+            </li>
+            <li>
+              <Link href={"/dashboard/user"}>User</Link>
+            </li>
+            <li>
+              <Link href={"/dashboard/profile"}>Profile</Link>
+            </li>
           </ul>
         </aside>
-        <div className="p-4">{children}</div>
+        <div className="p-4 max-h-screen overflow-x-auto">{children}</div>
       </div>
     </div>
   );
